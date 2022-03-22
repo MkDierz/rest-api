1098694f2c0c121e50e545d4fd6ea3f0f377da91
MkDierz <msyibbran24@gmail.com>
2022-03-22 23:28:37 +0700

    create middleware to check token on protected api

--- middleware/token.js
+++ middleware/token.js
@@ -1,10 +1,18 @@
+const { secretKey, jwt } = require('../config');
+
 function checkToken(req, res, next) {
   const { authorization } = req.headers;
   if (authorization) {
     const bearer = authorization.split(' ');
     const bearerToken = bearer[1];
-    req.token = bearerToken;
-    next();
+
+    jwt.verify(bearerToken, secretKey, (err, authData) => {
+      if (err) {
+        return res.status(403).send();
+      }
+      req.auth = authData;
+      return next();
+    });
   } else {
     // Forbidden
     res.sendStatus(403);
