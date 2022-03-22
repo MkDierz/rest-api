275119cf9025708cb612de23498ae565af5cd4f0
MkDierz <msyibbran24@gmail.com>
2022-03-22 23:29:00 +0700

    create auth controller and routes

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,22 +1,106 @@
-const register = (req, res) => {
-  const { userName, email } = req.body;
+/* eslint-disable no-shadow */
+/* eslint-disable consistent-return */
+const { User } = require('../models');
+const { secretKey, secretRefreshKey, jwt } = require('../config');
 
-  return res.status(200).json({
-    userName,
-    email,
-  });
+const register = (req, res) => {
+  const { userName, email, password } = req.body;
+  User
+    .findOne({
+      email
+    })
+    .then((data) => {
+      if (data) {
+        return res.status(400).send({
+          message: 'email exist'
+        });
+      }
+      User
+        .create({
+          userName,
+          email,
+          password
+        })
+        .then(() => res.status(200).send({
+          message: 'user registered',
+          email,
+          userName
+        }))
+        .catch((err) => res.send(err));
+    })
+    .catch((err) => res.send(err));
 };
 
 const login = (req, res) => {
   const { email, password } = req.body;
 
-  return res.status(200).send({
-    email,
-    password
+  User
+    .findOne({
+      email,
+    })
+    .then((userData) => {
+      if (!userData) {
+        return res.status(400).send({
+          message: 'email not found'
+        });
+      }
+      userData
+        .validPassword(password, userData.password)
+        .then((isAuth) => {
+          if (!isAuth) {
+            return res.send({
+              message: 'unauthorized'
+            });
+          }
+          const {
+            id,
+            userName,
+            email: userEmail,
+            userLevel
+          } = userData;
+
+          const tokenData = {
+            id,
+            userName,
+            email: userEmail,
+            userLevel
+          };
+
+          const token = jwt.sign(tokenData, secretKey, { expiresIn: '1d' });
+          const refreshToken = jwt.sign(tokenData, secretRefreshKey, { expiresIn: '7d' });
+
+          return res.send({
+            message: 'authorized',
+            token,
+            refreshToken
+          });
+        });
+    });
+};
+
+const refreshToken = (req, res) => {
+  const { refreshToken } = req.body;
+
+  jwt.verify(refreshToken, secretRefreshKey, (err, authData) => {
+    if (err) {
+      return res.status(403).send();
+    }
+    const data = authData;
+    delete data.exp;
+    delete data.iat;
+    const token = jwt.sign(data, secretKey, { expiresIn: '1d' });
+    const refreshToken = jwt.sign(data, secretKey, { expiresIn: '7d' });
+
+    return res.send({
+      message: 'token refreshed',
+      token,
+      refreshToken
+    });
   });
 };
 
 module.exports = {
   register,
-  login
+  login,
+  refreshToken
 };
--- routes/auth/index.js
+++ routes/auth/index.js
@@ -1,11 +1,12 @@
 const { Router } = require('express');
 const { auth } = require('../../controllers');
 
-const { register, login } = auth;
+const { register, login, refreshToken } = auth;
 
 const router = Router();
 
 router.post('/register', register);
 router.post('/login', login);
+router.post('/refresh-token', refreshToken);
 
 module.exports = router;
