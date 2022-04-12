607a383280219681636931cc8b16dac16e9fb06f
MkDierz <msyibbran24@gmail.com>
2022-04-12 22:33:06 +0700

    add environment to config

--- config/index.js
+++ config/index.js
@@ -1,9 +1,12 @@
+const jwt = require('jsonwebtoken');
+
 const secretKey = process.env.SECRET_KEY || 'secretKey';
 const secretRefreshKey = process.env.SECRET_REFRESH_KEY || 'secretRefreshKey';
-const jwt = require('jsonwebtoken');
+const environment = process.env.NODE_ENV;
 
 module.exports = {
   jwt,
   secretKey,
-  secretRefreshKey
+  secretRefreshKey,
+  environment
 };
