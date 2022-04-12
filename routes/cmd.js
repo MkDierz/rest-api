a3588dd65b5792555f528d6d087776a4871f6b3b
MkDierz <msyibbran24@gmail.com>
2022-04-12 22:33:38 +0700

    refactor command execute endpoint

--- routes/cmd.js
+++ routes/cmd.js
@@ -4,19 +4,52 @@ const { exec } = require('child_process');
 
 const router = Router();
 
-router.post('/migrate-prod', async (req, res) => {
-  await new Promise((resolve, reject) => {
-    const migrate = exec(
-      'sequelize db:migrate',
-      { env: process.env },
-      (err) => (err ? reject(err) : resolve())
-    );
+router.post('/db-migrate-up/:env', async (req, res) => {
+  const env = req.params.env || 'development';
+  let cmd = 'npx sequelize-cli db:migrate ';
+  if (env === 'production') {
+    cmd += '--env production';
+  }
+  try {
+    await new Promise((resolve, reject) => {
+      const migrate = exec(
+        cmd,
+        { env: process.env },
+        (err) => (err ? reject(err) : resolve())
+      );
 
-    // Forward stdout+stderr to this process
-    migrate.stdout.pipe(process.stdout);
-    migrate.stderr.pipe(process.stderr);
-  });
-  res.status(200).send({ message: 'database migration complete' });
+      // Forward stdout+stderr to this process
+      migrate.stdout.pipe(process.stdout);
+      migrate.stderr.pipe(process.stderr);
+    });
+    res.status(200).send({ message: 'database migration complete' });
+  } catch (error) {
+    res.status(500).send({ message: 'database migration failed', error });
+  }
+});
+
+router.post('/db-migrate-down/:env', async (req, res) => {
+  const env = req.params.env || 'development';
+  let cmd = 'npx sequelize-cli db:migrate:down ';
+  if (env === 'production') {
+    cmd += '--env production';
+  }
+  try {
+    await new Promise((resolve, reject) => {
+      const migrate = exec(
+        cmd,
+        { env: process.env },
+        (err) => (err ? reject(err) : resolve())
+      );
+
+      // Forward stdout+stderr to this process
+      migrate.stdout.pipe(process.stdout);
+      migrate.stderr.pipe(process.stderr);
+    });
+    res.status(200).send({ message: 'database migration complete' });
+  } catch (error) {
+    res.status(500).send({ message: 'database migration failed', error });
+  }
 });
 
 module.exports = router;
