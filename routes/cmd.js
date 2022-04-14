87cdb6be56872d41374ca6abd9d90c3e7a4aa0dc
MkDierz <msyibbran24@gmail.com>
2022-04-14 08:49:57 +0700

    move cmd logic to controller

--- routes/cmd.js
+++ routes/cmd.js
@@ -1,55 +1,31 @@
 const { Router } = require('express');
-// const { commands } = require('npm');
-const { exec } = require('child_process');
+const { check } = require('express-validator');
+const { cmd } = require('../controllers');
+const { handleValidationErrors } = require('../middleware/validator');
+const { checkToken } = require('../middleware/token');
+const { environment } = require('../config');
 
 const router = Router();
 
-router.post('/db-migrate-up/:env', async (req, res) => {
-  const env = req.params.env || 'development';
-  let cmd = 'npx sequelize-cli db:migrate ';
-  if (env === 'production') {
-    cmd += '--env production';
-  }
-  try {
-    await new Promise((resolve, reject) => {
-      const migrate = exec(
-        cmd,
-        { env: process.env },
-        (err) => (err ? reject(err) : resolve())
-      );
+const {
+  up, down, pending, executed
+} = cmd;
 
-      // Forward stdout+stderr to this process
-      migrate.stdout.pipe(process.stdout);
-      migrate.stderr.pipe(process.stderr);
-    });
-    res.status(200).send({ message: 'database migration complete' });
-  } catch (error) {
-    res.status(500).send({ message: 'database migration failed', error });
-  }
-});
+const checkEnv = [
+  check('env')
+    .exists()
+    .withMessage('enviroment is required')
+    .isLength({ min: 3 })
+    .withMessage('wrong userName length')
+];
 
-router.post('/db-migrate-down/:env', async (req, res) => {
-  const env = req.params.env || 'development';
-  let cmd = 'npx sequelize-cli db:migrate:down ';
-  if (env === 'production') {
-    cmd += '--env production';
-  }
-  try {
-    await new Promise((resolve, reject) => {
-      const migrate = exec(
-        cmd,
-        { env: process.env },
-        (err) => (err ? reject(err) : resolve())
-      );
+if (environment !== 'development') {
+  router.use(checkToken);
+}
 
-      // Forward stdout+stderr to this process
-      migrate.stdout.pipe(process.stdout);
-      migrate.stderr.pipe(process.stderr);
-    });
-    res.status(200).send({ message: 'database migration complete' });
-  } catch (error) {
-    res.status(500).send({ message: 'database migration failed', error });
-  }
-});
+router.post('/db/migrate-up/:env', checkEnv, handleValidationErrors, up);
+router.post('/db/migrate-down/:env', checkEnv, handleValidationErrors, down);
+router.post('/db/migrate-pending/:env', checkEnv, handleValidationErrors, pending);
+router.post('/db/migrate-executed/:env', checkEnv, handleValidationErrors, executed);
 
 module.exports = router;
