f68e4a7582a7bd6f918abbdd9da13a3ccd5705f8
MkDierz <msyibbran24@gmail.com>
2022-04-14 08:48:31 +0700

    create cmd controller

---
+++ controllers/cmd.js
@@ -0,0 +1,124 @@
+/* eslint-disable consistent-return */
+/* eslint-disable global-require */
+/* eslint-disable import/no-dynamic-require */
+const { Sequelize } = require('sequelize');
+const { Umzug, SequelizeStorage } = require('umzug');
+
+const config = require('../config/config.json');
+
+const umzug = (env) => {
+  const sequelize = new Sequelize(env.database, env.username, env.password, env);
+  return new Umzug({
+    migrations: {
+      glob: 'migrations/*.js',
+      resolve: ({ name, path, context }) => {
+        const migration = require(path);
+        return {
+          // adjust the parameters Umzug will
+          // pass to migration methods when called
+          name,
+          up: async () => migration.up(context, Sequelize),
+          down: async () => migration.down(context, Sequelize),
+        };
+      },
+    },
+    context: sequelize.getQueryInterface(),
+    storage: new SequelizeStorage({ sequelize }),
+    logger: console,
+  });
+};
+
+const returnMsg = ({
+  msg, env, envConfig, data
+} = {}) => {
+  const retVal = {};
+  if (msg) {
+    retVal.message = msg;
+  }
+  if (env) {
+    retVal.connection = {
+      env
+    };
+  }
+  if (envConfig) {
+    retVal.connection = {
+      ...retVal.connection,
+      ...envConfig,
+    };
+  }
+  if (data) {
+    retVal.data = data;
+  }
+  return retVal;
+};
+
+async function up(req, res) {
+  const env = req.params.env || 'development';
+  const envConfig = config[env];
+  const conn = umzug(envConfig);
+  try {
+    await conn
+      .up()
+      .then((migrations) => res
+        .status(200)
+        .send(
+          returnMsg({
+            msg: 'database migration executed', env, envConfig, data: migrations
+          })
+        ));
+  } catch (error) {
+    return res.status(500).send({ message: error.message });
+  }
+}
+
+async function down(req, res) {
+  try {
+    const env = req.params.env || 'development';
+    const envConfig = config[env];
+    const conn = umzug(envConfig);
+    return await conn
+      .down()
+      .then((migrations) => res
+        .status(200)
+        .send(
+          returnMsg({
+            msg: 'database migration reverted', env, envConfig, data: migrations
+          })
+        ));
+  } catch (error) {
+    return res.status(500).send({ message: error.message });
+  }
+}
+
+async function pending(req, res) {
+  try {
+    const env = req.params.env || 'development';
+    const envConfig = config[env];
+    const conn = umzug(envConfig);
+    return await conn
+      .pending()
+      .then((migrations) => res.status(200).send(returnMsg({ data: migrations })));
+  } catch (error) {
+    return res.status(500).send({ message: error.message });
+  }
+}
+
+async function executed(req, res) {
+  try {
+    const env = req.params.env || 'development';
+    const envConfig = config[env];
+    const conn = umzug(envConfig);
+    return await conn
+      .executed()
+      .then((migrations) => res.status(200).send(returnMsg({ data: migrations })));
+  } catch (error) {
+    return res.status(500).send({ message: error.message });
+  }
+}
+
+module.exports = {
+  up,
+  down,
+  pending,
+  executed
+};
--- controllers/index.js
+++ controllers/index.js
@@ -1,4 +1,5 @@
 const auth = require('./auth');
 const api = require('./api');
+const cmd = require('./cmd');
 
-module.exports = { auth, api };
+module.exports = { auth, api, cmd };
