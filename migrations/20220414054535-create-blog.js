4d24c5e7d210e1c4c8816a0eb763de678cd1a9e9
MkDierz <msyibbran24@gmail.com>
2022-04-14 15:50:29 +0700

    create table blog

---
+++ migrations/20220414054535-create-blog.js
@@ -0,0 +1,40 @@
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('Blogs', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      user_id: {
+        allowNull: false,
+        type: Sequelize.INTEGER
+      },
+      blogTitle: {
+        allowNull: false,
+        type: Sequelize.STRING
+      },
+      blogContent: {
+        allowNull: false,
+        type: Sequelize.TEXT
+      },
+      blogPublic: {
+        allowNull: false,
+        type: Sequelize.BOOLEAN,
+        defaultValue: false,
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  async down(queryInterface) {
+    await queryInterface.dropTable('Blogs');
+  }
+};
---
+++ models/blog.js
@@ -0,0 +1,29 @@
+const {
+  Model
+} = require('sequelize');
+
+module.exports = (sequelize, DataTypes) => {
+  class Blog extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      Blog.belongsTo(models.User, {
+        foreignKey: 'user_id',
+        as: 'user'
+      });
+    }
+  }
+  Blog.init({
+    user_id: DataTypes.INTEGER,
+    blogTitle: DataTypes.STRING,
+    blogContent: DataTypes.TEXT,
+    blogPublic: DataTypes.BOOLEAN
+  }, {
+    sequelize,
+    modelName: 'Blog',
+  });
+  return Blog;
+};
