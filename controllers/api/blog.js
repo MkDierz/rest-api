d74f8cc54d47a9bae1074a001c9dcb10e6a813ec
MkDierz <msyibbran24@gmail.com>
2022-12-15 02:34:27 +0700

    update blog api

--- controllers/api/blog.js
+++ controllers/api/blog.js
@@ -2,29 +2,76 @@ const { Blog, User } = require('../../models');
 
 function get(req, res) {
   Blog
-    .findAll({
-      user_id: req.auth.id,
-      include: [{
-        model: User,
-        as: 'user'
-      }],
+    .findAndCountAll({
+
     })
-    .then((blog) => {
-      res.send(blog);
+    .then((data) => {
+      res.json(data);
     });
 }
 
 function post(req, res) {
   const { id } = req.auth;
-  Blog.create({
-    ...req.body,
-    user_id: id
-  }).then(() => {
-    res.sendStatus(200);
-  });
+  Blog
+    .create({
+      user_id: id,
+      ...req.body,
+    })
+    .then(() => res.sendStatus(201));
+}
+
+function getId(req, res) {
+  const { id } = req.params;
+  Blog
+    .findByPk(id, {
+      include: [
+        {
+          model: User,
+          as: 'user',
+        },
+      ],
+    })
+    .then((data) => {
+      res.json(data);
+    });
+}
+
+function deleteId(req, res) {
+  const { id } = req.params;
+
+  return Blog
+    .findByPk(id)
+    .then((data) => {
+      if (!data) {
+        return res.sendStatus(404);
+      }
+      return data
+        .destroy()
+        .then(() => res.sendStatus(204));
+    });
+}
+
+function putId(req, res) {
+  const { id } = req.params;
+
+  return Blog
+    .findByPk(id)
+    .then((data) => {
+      if (!data) {
+        return res.sendStatus(404);
+      }
+      return data
+        .update({
+          ...req.body,
+        })
+        .then(() => res.sendStatus(200));
+    });
 }
 
 module.exports = {
   get,
-  post
+  getId,
+  post,
+  putId,
+  deleteId,
 };
